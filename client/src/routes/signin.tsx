0d5722ab87797703ba042b87ebfd5d47c3680010
Damien Dumontet <damiendumontet@192.168.68.105>
2025-05-12 17:04:41 -0300

    add otp input

--- client/src/routes/signin.tsx
+++ client/src/routes/signin.tsx
@@ -2,20 +2,57 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { authClient } from '@/lib/auth';
 import { createFileRoute } from '@tanstack/react-router';
+import { useState } from 'react';
+import { motion, useAnimate } from 'motion/react';
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from '@/components/ui/input-otp';
 
 export const Route = createFileRoute('/signin')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const [email, setEmail] = useState('');
+  const [showOtp, setShowOtp] = useState(false);
+  const [emailFormRef, animateEmailForm] = useAnimate();
+  const [otpFormRef, animateOtpForm] = useAnimate();
+
   const handleSigninWithEmail = async () => {
+    if (!email) return;
+
     const { data, error } = await authClient.emailOtp.sendVerificationOtp({
-      email: 'user-email@email.com',
+      email: email,
       type: 'sign-in',
     });
 
     console.log('DATA: ', data);
     console.log('ERROR: ', error);
+
+    await animateEmailForm(
+      emailFormRef.current,
+      { y: -20, opacity: 0 },
+      { duration: 0.3 },
+    );
+
+    setShowOtp(true);
+  };
+
+  const handleVerifyOtp = async (value: string) => {
+    console.log('OTP value:', value);
+  };
+
+  const handleBackToEmail = async () => {
+    await animateOtpForm(
+      otpFormRef.current,
+      { y: -20, opacity: 0 },
+      { duration: 0.3 },
+    );
+
+    setShowOtp(false);
   };
 
   return (
@@ -26,22 +63,75 @@ function RouteComponent() {
           <div className="flex flex-col gap-1">
             <h2 className="text-5xl font-bold font-display">Welcome back</h2>
             <div className="text-lg text-muted-foreground pl-2">
-              Enter you email to receive a one time pass code.
+              Enter your email to receive a one-time pass code.
             </div>
           </div>
-          <div className="flex flex-col gap-4">
-            <Input type="email" placeholder="Email" />
-            <Button onClick={handleSigninWithEmail}>sign-in with email</Button>
-          </div>
-          <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-            <span className="relative z-10 bg-background px-2 text-muted-foreground">
-              Or continue with
-            </span>
-          </div>
-          <div className="flex gap-4">
-            <Button className="flex-1">sign-in with apple</Button>
-            <Button className="flex-1">sign-in with google</Button>
-          </div>
+
+          {!showOtp && (
+            <motion.div
+              ref={emailFormRef}
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.3, easing: 'ease-out', delay: 0.2 }}
+              className="flex flex-col gap-4 h-[300px]"
+            >
+              <Input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Button onClick={handleSigninWithEmail}>
+                sign-in with email
+              </Button>
+
+              <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+                <span className="relative z-10 bg-background px-2 text-muted-foreground">
+                  Or continue with
+                </span>
+              </div>
+              <div className="flex gap-4">
+                <Button className="flex-1">sign-in with apple</Button>
+                <Button className="flex-1">sign-in with google</Button>
+              </div>
+            </motion.div>
+          )}
+
+          {showOtp && (
+            <motion.div
+              ref={otpFormRef}
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.3, easing: 'ease-out', delay: 0.2 }}
+              className="flex flex-col gap-8 h-[300px]"
+            >
+              <div className="flex">
+                <InputOTP maxLength={6} onComplete={handleVerifyOtp}>
+                  <InputOTPGroup>
+                    <InputOTPSlot index={0} />
+                    <InputOTPSlot index={1} />
+                    <InputOTPSlot index={2} />
+                  </InputOTPGroup>
+                  <InputOTPSeparator />
+                  <InputOTPGroup>
+                    <InputOTPSlot index={3} />
+                    <InputOTPSlot index={4} />
+                    <InputOTPSlot index={5} />
+                  </InputOTPGroup>
+                </InputOTP>
+              </div>
+              <div className="flex gap-4">
+                <Button
+                  className="flex-1"
+                  variant="outline"
+                  onClick={handleBackToEmail}
+                >
+                  Back to email
+                </Button>
+                <Button className="flex-1">Verify</Button>
+              </div>
+            </motion.div>
+          )}
         </div>
       </div>
       <div className="w-full h-full p-8">
