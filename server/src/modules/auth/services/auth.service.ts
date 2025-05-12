e0c6fa10f91346292721bf088d08f6be48cff879
Damien Dumontet <damiendumontet@192.168.68.105>
2025-05-12 11:25:58 -0300

    add email otp plugin

--- server/src/modules/auth/services/auth.service.ts
+++ server/src/modules/auth/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from 'better-auth';
 import { Pool } from 'pg';
 import 'dotenv/config';
+import { emailOTP } from 'better-auth/plugins';
 
 export const auth = betterAuth({
   database: new Pool({
@@ -10,4 +11,13 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  plugins: [
+    emailOTP({
+      async sendVerificationOTP({ email, otp, type }) {
+        console.log(
+          `send otp email to ${email} with the code ${otp} to ${type}.`,
+        );
+      },
+    }),
+  ],
 });
