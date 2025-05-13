0bbb089302cd1322e479ff8cc87b8fdab86a39bf
Damien Dumontet <damiendumontet@192.168.68.105>
2025-05-13 16:50:06 -0300

    add resend email api

--- server/src/modules/auth/services/auth.service.ts
+++ server/src/modules/auth/services/auth.service.ts
@@ -2,6 +2,7 @@ import { betterAuth } from 'better-auth';
 import { Pool } from 'pg';
 import 'dotenv/config';
 import { emailOTP } from 'better-auth/plugins';
+import { Resend } from 'resend';
 
 export const auth = betterAuth({
   database: new Pool({
@@ -13,10 +14,15 @@ export const auth = betterAuth({
   },
   plugins: [
     emailOTP({
-      async sendVerificationOTP({ email, otp, type }) {
-        console.log(
-          `send otp email to ${email} with the code ${otp} to ${type}.`,
-        );
+      async sendVerificationOTP({ email, otp }) {
+        const resend = new Resend(process.env.RESEND_KEY);
+
+        resend.emails.send({
+          from: 'onboarding@resend.dev',
+          to: email,
+          subject: `fiqo - ${otp} is your verification code`,
+          html: `<p>Enter the following verification code when prompted: ${otp}</p>`,
+        });
       },
     }),
   ],
