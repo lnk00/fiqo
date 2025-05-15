10013eabe9979233be08479e0e31bcb8f529189f
Damien Dumontet <damiendumontet@192.168.68.105>
2025-05-15 10:46:19 -0300

    modify otp design

--- client/src/modules/auth/components/otp-form.tsx
+++ client/src/modules/auth/components/otp-form.tsx
@@ -26,37 +26,29 @@ export function OtpForm({
       transition={{ duration: 0.3, easing: 'ease-out', delay: 0.2 }}
       className="flex flex-col gap-8 h-[300px]"
     >
-      <div className="flex">
-        <InputOTP maxLength={6} onComplete={handleVerifyOtp}>
-          <InputOTPGroup>
-            <InputOTPSlot index={0} />
-            <InputOTPSlot index={1} />
-            <InputOTPSlot index={2} />
-          </InputOTPGroup>
-          <InputOTPSeparator />
-          <InputOTPGroup>
-            <InputOTPSlot index={3} />
-            <InputOTPSlot index={4} />
-            <InputOTPSlot index={5} />
-          </InputOTPGroup>
-        </InputOTP>
-      </div>
-      <div className="relative flex gap-4">
+      <div className="flex gap-2">
         <button
-          className="btn btn-lg flex-1"
+          className="btn btn-square btn-lg"
           type="button"
           onClick={handleBackToEmail}
         >
           <ArrowLeft />
-          back to email
         </button>
-        <button
-          className="btn btn-lg btn-neutral flex-1"
-          type="button"
-          onClick={handleBackToEmail}
-        >
-          verify
-        </button>
+        <div className="flex">
+          <InputOTP maxLength={6} onComplete={handleVerifyOtp}>
+            <InputOTPGroup>
+              <InputOTPSlot index={0} />
+              <InputOTPSlot index={1} />
+              <InputOTPSlot index={2} />
+            </InputOTPGroup>
+            <InputOTPSeparator />
+            <InputOTPGroup>
+              <InputOTPSlot index={3} />
+              <InputOTPSlot index={4} />
+              <InputOTPSlot index={5} />
+            </InputOTPGroup>
+          </InputOTP>
+        </div>
       </div>
     </motion.div>
   );
